c2e67cd5a684243c75123c5051d9f1f0281c942a
raku <lakjinteo@gmail.com>
2023-07-15 22:04:09 +0800

    finalises card design

--- app/api/listing/route.jsx
+++ app/api/listing/route.jsx
@@ -1,6 +1,8 @@
 import { connectToDB } from "@/utils/database"
 import Account from "@/models/account"
 import Listing from "@/models/listing"
+import UserManual from "@/models/usermanual"
+import UserOAuth from "@/models/useroauth"
 
 
 export const POST = async (req,res) => {
@@ -38,13 +40,19 @@ export const GET = async (req,res) => {
     try {
         if (req.nextUrl.searchParams.get('usermanual')) {
             const account = await Account.findOne({usermanual:req.nextUrl.searchParams.get('usermanual')})
-            const listings = await Listing.find({seller:account._id})
-            return new Response(JSON.stringify(listings),{status:200})
+            const listings = await Listing.find({seller:account._id}).populate('seller')
+            const listings2 = await UserManual.populate(listings,{
+                path:"seller.usermanual", select: 'username'
+            })
+            return new Response(JSON.stringify(listings2),{status:200})
         }
         if (req.nextUrl.searchParams.get('useroauth')) {
             const account = await Account.findOne({useroauth:req.nextUrl.searchParams.get('useroauth')})
-            const listings = await Listing.find({seller:account._id})
-            return new Response(JSON.stringify(listings),{status:200})
+            const listings = await Listing.find({seller:account._id}).populate('seller')
+            const listings2 = await UserOAuth.populate(listings,{
+                path:"seller.useroauth", select: 'username'
+            })
+            return new Response(JSON.stringify(listings2),{status:200})
         }
     } catch (err) {
         return new Response("Failed request.",{status:500})
--- components/ListingCard.jsx
+++ components/ListingCard.jsx
@@ -5,23 +5,38 @@ export default function ListingCard({item}) {
 
 
     return (
-        <div className='listing-card'>
-            <div className='heart'><Heart/></div>
-            <div className='listing-content'>
-                <div className='image-container'>
-                    <Image style={{width:'100%', aspectRatio:'1/1', objectFit:'cover'}} alt='thumbnail' src={item.listingthumbnail} width={150} height={150} />
-                </div>
-                <div className='text-content'>
-                    <div className='text-content-inner'>
-                        <h2>{item.listingname}</h2>
-                        <p>{item.listingdescription}</p>
-                        <p className='price'>{item.listingprice}</p>
-                    </div>
-                    <div className='seller-container'>
-                        <p>Sold by {item.seller}</p>
-                    </div>
-                </div>
-            </div>
+        // <div className='listing-card'>
+        //     <div className='heart'><Heart/></div>
+        //     <div className='listing-content'>
+        //         <div className='image-container'>
+                    // <Image style={{ aspectRatio:'1/1', objectFit:'cover', borderRadius:'15px'}} 
+                    // alt='thumbnail' src={item.listingthumbnail} width={190} height={190} />
+        //         </div>
+        //         <div className='text-content'>
+        //             <div className='text-content-inner'>
+        //                 <h2>{item.listingname}</h2>
+        //                 <p>{item.listingdescription}</p>
+        //                 <p className='pricee'>{item.listingprice}</p>
+        //             </div>
+        //             <div className='seller-container'>
+        //                 <p>Sold by {item.seller.usermanual?.username || item.seller.useroauth?.username}</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        // card design from https://uiverse.io/Sashank02/new-warthog-10 
+
+        <div class="card">
+        <div class="card-image"><Image style={{ objectFit:'cover', borderRadius:'15px'}} 
+                    alt='thumbnail' src={item.listingthumbnail} fill /></div>
+        <p class="card-title">{item.listingname}</p>
+        <p class="card-body">
+            {item.listingdescription}
+        </p>
+        <p class="footer">Sold by <span class="by-name">{item.seller.usermanual?.username || item.seller.useroauth?.username}</span> at <span class="date">${item.listingprice}</span></p>
         </div>
+
+
     )
 }
