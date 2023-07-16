c06b0c417a9be479ab2f74f4ab304d2514fd528f
raku <lakjinteo@gmail.com>
2023-07-16 16:43:22 +0800

    backup

--- components/ListingCard.jsx
+++ components/ListingCard.jsx
@@ -65,29 +65,6 @@ export default function ListingCard({item,usermanual,useroauth,usermanualaccount
     console.log(item.favby.includes(usermanualaccount), item.favby.includes(useroauthaccount))
 
     return (
-        // <div className='listing-card'>
-        //     <div className='heart'><Heart/></div>
-        //     <div className='listing-content'>
-        //         <div className='image-container'>
-                    // <Image style={{ aspectRatio:'1/1', objectFit:'cover', borderRadius:'15px'}} 
-                    // alt='thumbnail' src={item.listingthumbnail} width={190} height={190} />
-        //         </div>
-        //         <div className='text-content'>
-        //             <div className='text-content-inner'>
-        //                 <h2>{item.listingname}</h2>
-        //                 <p>{item.listingdescription}</p>
-        //                 <p className='pricee'>{item.listingprice}</p>
-        //             </div>
-        //             <div className='seller-container'>
-        //                 <p>Sold by {item.seller.usermanual?.username || item.seller.useroauth?.username}</p>
-        //             </div>
-        //         </div>
-        //     </div> 
-        // </div> ((usermanual || useroauth) || (usermanual != item.seller.usermanual?._id || useroauth != item.seller.useroauth?._id))
-
-        // card design from https://uiverse.io/Sashank02/new-warthog-10 
-
-
         <div className="card" key={key} onClick={() => handleGoToListing(item._id)}>
             {(!(usermanual || useroauth)) ?
             <>
@@ -114,7 +91,27 @@ export default function ListingCard({item,usermanual,useroauth,usermanualaccount
             </p>
             <p className="footer">Listed by <span className="by-name">{item.seller.usermanual?.username || item.seller.useroauth?.username}</span> at <span className="date">${item.listingprice}</span></p>
         </div>
-
-
     )
 }
+
+        // <div className='listing-card'>
+        //     <div className='heart'><Heart/></div>
+        //     <div className='listing-content'>
+        //         <div className='image-container'>
+                    // <Image style={{ aspectRatio:'1/1', objectFit:'cover', borderRadius:'15px'}} 
+                    // alt='thumbnail' src={item.listingthumbnail} width={190} height={190} />
+        //         </div>
+        //         <div className='text-content'>
+        //             <div className='text-content-inner'>
+        //                 <h2>{item.listingname}</h2>
+        //                 <p>{item.listingdescription}</p>
+        //                 <p className='pricee'>{item.listingprice}</p>
+        //             </div>
+        //             <div className='seller-container'>
+        //                 <p>Sold by {item.seller.usermanual?.username || item.seller.useroauth?.username}</p>
+        //             </div>
+        //         </div>
+        //     </div> 
+        // </div> ((usermanual || useroauth) || (usermanual != item.seller.usermanual?._id || useroauth != item.seller.useroauth?._id))
+
+        // card design from https://uiverse.io/Sashank02/new-warthog-10 
