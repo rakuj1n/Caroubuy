2caadc7c79792f700da5288690df683eae89b5f4
raku <lakjinteo@gmail.com>
2023-07-19 21:02:07 +0800

    tries to fix localstorage problem

--- components/useLocalStorage.jsx
+++ components/useLocalStorage.jsx
@@ -18,5 +18,22 @@ export function useLocalStorage(key, initialValue) {
         localStorage.setItem(key, JSON.stringify(value))
     },[key,value])
 
+    // -------------------
+
+    useEffect(() => {
+        if (typeof localStorage !== 'undefined') {
+          const storedValue = localStorage.getItem(key)
+          setValue(storedValue ? JSON.parse(storedValue) : initialValue)
+        }
+      }, []);
+    
+      useEffect(() => {
+        if (typeof localStorage !== 'undefined') {
+          localStorage.setItem(key, JSON.stringify(value))
+        }
+      }, [key, value])
+
+      // -------------------
+
     return [value,setValue]
 }
