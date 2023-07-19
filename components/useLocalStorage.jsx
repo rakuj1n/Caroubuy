f481415866bb4f041a2ba7ef4f17cea250663256
raku <lakjinteo@gmail.com>
2023-07-19 21:06:36 +0800

    tries to fix localstorage v 2

--- components/useLocalStorage.jsx
+++ components/useLocalStorage.jsx
@@ -3,37 +3,72 @@
 import { useEffect, useState } from "react"
 
 export function useLocalStorage(key, initialValue) {
-    const [value,setValue] = useState(() => {
-        const jsonValue = localStorage.getItem(key)
-        if (jsonValue != null) return JSON.parse(jsonValue)
+  const [value, setValue] = useState(() => {
+    if (typeof localStorage !== 'undefined') {
+      const storedValue = localStorage.getItem(key)
+      if (storedValue !== null) {
+        return JSON.parse(storedValue)
+      }
+    }
 
-        if (typeof initialValue === 'function') {
-            return initialValue()
-        } else {
-            return initialValue
-        }
-    })
+    if (typeof initialValue === 'function') {
+      return initialValue()
+    } else {
+      return initialValue
+    }
+  })
 
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(value))
-    },[key,value])
+  useEffect(() => {
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+  }, [key, value])
 
-    // -------------------
-
-    useEffect(() => {
-        if (typeof localStorage !== 'undefined') {
-          const storedValue = localStorage.getItem(key)
-          setValue(storedValue ? JSON.parse(storedValue) : initialValue)
-        }
-      }, []);
-    
-      useEffect(() => {
-        if (typeof localStorage !== 'undefined') {
-          localStorage.setItem(key, JSON.stringify(value))
-        }
-      }, [key, value])
-
-      // -------------------
-
-    return [value,setValue]
+  return [value, setValue]
 }
+
+
+
+
+
+
+
+// 'use client'
+
+// import { useEffect, useState } from "react"
+
+// export function useLocalStorage(key, initialValue) {
+//     const [value,setValue] = useState(() => {
+//         const jsonValue = localStorage.getItem(key)
+//         if (jsonValue != null) return JSON.parse(jsonValue)
+
+//         if (typeof initialValue === 'function') {
+//             return initialValue()
+//         } else {
+//             return initialValue
+//         }
+//     })
+
+//     useEffect(() => {
+//         localStorage.setItem(key, JSON.stringify(value))
+//     },[key,value])
+
+//     // -------------------
+
+//     useEffect(() => {
+//         if (typeof localStorage !== 'undefined') {
+//           const storedValue = localStorage.getItem(key)
+//           setValue(storedValue ? JSON.parse(storedValue) : initialValue)
+//         }
+//       }, []);
+    
+//       useEffect(() => {
+//         if (typeof localStorage !== 'undefined') {
+//           localStorage.setItem(key, JSON.stringify(value))
+//         }
+//       }, [key, value])
+
+//       // -------------------
+
+//     return [value,setValue]
+// }
