77669c7ea590f99289eea37e9434e505c3b774c7
raku <lakjinteo@gmail.com>
2023-07-20 17:07:40 +0800

    tries removing initial attempt in next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  runtime: 'edge', // for Edge API Routes only
-  unstable_allowDynamic: [
-    '/lib/utilities.js', // allows a single file
-    '/node_modules/function-bind/**', // use a glob to allow anything in the function-bind 3rd party module
-  ],
   eslint: {
     ignoreDuringBuilds: true,
   },
